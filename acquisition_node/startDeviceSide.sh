f614bde5dadb9372b676ac7c462a4f9827ec0c82
Aleksandar Petrov <aleksanndar1995@gmail.com>
2018-12-12 15:46:12 +0100

    Acquisition with multiprocessing instead of sockets

--- acquisition_node/startDeviceSide.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-
-export ROS_MASTER_URI=http://${ACQ_ROS_MASTER_URI_DEVICE}:${ACQ_ROS_MASTER_URI_DEVICE_PORT}
-./deviceSideSubscriber.py
--- acquisition_node/startServerSide.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-
-export ROS_MASTER_URI=http://${ACQ_ROS_MASTER_URI_SERVER}:${ACQ_ROS_MASTER_URI_SERVER_PORT}
-python serverSidePublisher.py
