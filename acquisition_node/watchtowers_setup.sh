0c20c430b000d743311bb512aa3feef855913345
Aleksandar Petrov <aleksanndar1995@gmail.com>
2018-12-16 12:51:06 +0100

    acquisition node saturday updates

---
+++ acquisition_node/watchtowers_setup.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+# ALWAYS SET THESE TWO:
+SERVER_HOSTNAME=ubuntu-Aleks.local
+SERVER_IP=192.168.1.104
+
+array=(demowatchtower12 demowatchtower13 demowatchtower14 demowatchtower15 demowatchtower06 demowatchtower01 demowatchtower02 demowatchtower03 demowatchtower04 demowatchtower05 demowatchtower07 demowatchtower08 demowatchtower09 demowatchtower10)
+
+echo "We are setting up ${#array[*]} watchtowers"
+
+for index in ${!array[*]}
+do
+    printf "\nSetting up %s\n" ${array[$index]}
+    docker -H ${array[$index]}.local start roscore
+    docker -H ${array[$index]}.local start ros-picam
+    docker -H ${array[$index]}.local stop cslam-aquisition || echo "Didn't stop older cslam-aquisition, probably doesn't exist, so don't worry."
+    docker -H ${array[$index]}.local rm cslam-aquisition || echo "Didn't remove older cslam-aquisition, probably doesn't exist, so don't worry."
+    docker -H ${array[$index]}.local run -d --name cslam-aquisition --restart always --network=host -e ACQ_ROS_MASTER_URI_DEVICE=${array[$index]}.local -e ACQ_ROS_MASTER_URI_DEVICE_IP=127.0.0.1 -e ACQ_ROS_MASTER_URI_SERVER=${SERVER_HOSTNAME} -e ACQ_ROS_MASTER_URI_SERVER_IP=${SERVER_IP}  -e ACQ_DEVICE_NAME=${array[$index]} -e ACQ_BEAUTIFY=0 -e ACQ_STATIONARY_ODOMETRY=0 -e ACQ_ODOMETRY_UPDATE_RATE=0 -e ACQ_POSES_UPDATE_RATE=15 -e ACQ_TEST_STREAM=1 -e ACQ_TOPIC_VELOCITY_TO_POSE=velocity_to_pose_node/pose -e ACQ_TOPIC_RAW=camera_node/image/compressed aleksandarpetrov/cslam-aquisition-rpi
+done
