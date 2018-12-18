9ad529ea10558f710ce4815e0fa2b28db61ab5b1
Rohit Suri <rohitsuri.nitr@gmail.com>
2018-12-18 14:23:10 +0100

    Diagnostics dockerfile updated

--- ros-cslam/wrapper_diagnostics.sh
+++ ros-cslam/wrapper_diagnostics.sh
@@ -3,5 +3,8 @@
 # Ensure that the hostnames and ip address are setup in /etc/hosts
 echo "${ROS_MASTER_URI_DEVICE_IP} ${ROS_MASTER_URI_DEVICE}" >> /etc/hosts
 
+export ROS_MASTER_URI=http://${ROS_MASTER_URI_DEVICE}:11311
+export ROS_IP=${ROS_MASTER_URI_DEVICE_IP}
+
 # Start the diagnostics
 rosrun cslam_diagnostics cslam_diagnostics_node.py
