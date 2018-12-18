33faf634a8f673dacaf9a501a04efc0205362028
BensonKuan <bensonkuan1990@gmail.com>
2018-12-18 16:02:35 +0100

    dockerization for graph optimizer

---
+++ ros-cslam/src/pose_graph_builder/wrapper.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+# Ensure that the hostnames and ip address are setup in /etc/hosts
+echo "${ROS_MASTER_URI_DEVICE_IP} ${ROS_MASTER_URI_DEVICE}" >> /etc/hosts
+
+export ROS_MASTER_URI=http://${ROS_MASTER_URI_DEVICE}:11311
+export ROS_IP=${ROS_MASTER_URI_DEVICE_IP}
+
+# Start the diagnostics
+roslaunch pose_graph_builder grapher_builder.launch
+
