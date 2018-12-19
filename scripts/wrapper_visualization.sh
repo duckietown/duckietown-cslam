7ca932bfcfc3b58a860572c5fff51a3e53a80b03
AmaurX <amaury.camus92@gmail.com>
2018-12-19 17:54:46 +0100

    updates

--- scripts/wrapper_visualization.sh
+++ scripts/wrapper_visualization.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 
 # Ensure that the hostnames and ip address are setup in /etc/hosts
-echo "${ROS_MASTER_URI_DEVICE_IP} ${ROS_MASTER_URI_DEVICE}" >> /etc/hosts
+echo "${ROS_MASTER_IP} ${ROS_MASTER}" >> /etc/hosts
 
-export ROS_MASTER_URI=http://${ROS_MASTER_URI_DEVICE}:11311
+export ROS_MASTER_URI=http://${ROS_MASTER}:11311
 
 # Start the diagnostics
 roslaunch duckietown_visualization publish_map.launch rviz_config:=/catkin-ws/cSLAM.rviz
